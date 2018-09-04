63f56d49eb39305e4248ab6e37e9e28bc2947dd1
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-04 10:58:31 -0500

    added Film Page Component

---
+++ src/components/Films.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import 'es6-promise';
+import Card from './Card';
+
+class films extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {};
+    }
+
+    componentDidMount() {
+        fetch("https://ghibliapi.herokuapp.com/films")
+            .then(res => res.json())
+            .then(obj => {
+                //mapping over recieved array to produce Film specific Card components
+                let filmsData = obj.map((val) => {
+                    return <Card key={val.id} title={val.title} secondary={val.description} third={val.director} />;
+                });
+                console.log(filmsData);
+                this.setState({ filmCards: filmsData });
+            });
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+            {this.state.filmCards}
+            </div>
+        );
+    }
+}
+
+export default films;
