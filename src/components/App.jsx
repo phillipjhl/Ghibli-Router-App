2e8575a5a9919cd9f7f3d57aafd548795dd707c8
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-04 11:16:36 -0500

    sample render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import React, {Component, Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-class App extends Components {
-    constructor(props) {
-        super(props);
-    }
+class App extends Component {
 
     render() {
+        return (
         <Router>
             <Fragment>
-                
+                <h1 className="text-primary">Hello Word!</h1>
             </Fragment>
         </Router>
+        );
     }
 }
+
+export default App;
