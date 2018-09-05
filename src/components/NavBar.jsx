e12ca36a747aa9d2110f43d605e4f6fb4f8d0d14
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-04 23:55:07 -0500

    added new brand name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 function NavBar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-            <Link to='/' className="navbar-brand" >Ghibli</Link>
+            <Link to='/' className="navbar-brand" >Ghibli Wiki</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
