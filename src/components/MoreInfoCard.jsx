4874fcdead8a0be46f2a598fca8d358b0a63362d
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-04 23:54:33 -0500

    added titles

--- src/components/MoreInfoCard.jsx
+++ src/components/MoreInfoCard.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 function MoreInfoCard(props) {
     return (
+        //for a film
         <div className="card" >
                 <div className="card-body">
                     <h5 className="card-header text-dark">{props.first}</h5>
                     <p className="card-text">{props.second}</p>
-                    <p className="card-text">{props.third}</p>
-                    <p className="car-text">{props.fourth}</p>
-                    <p className="car-text">{props.fifth}</p>
-                    <p className="car-text">{props.sixth}</p>
+                    <p className="card-text">Director: {props.third}</p>
+                    <p className="car-text">Producer: {props.fourth}</p>
+                    <p className="car-text">Release Date; {props.fifth}</p>
+                    <p className="car-text">Rotten Tomato Score: {props.sixth}%</p>
                 </div>
             </div>
     );
