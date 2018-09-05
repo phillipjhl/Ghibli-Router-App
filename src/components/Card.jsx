d85b47bdaca9c9b43c5490b28ae9f454441f3741
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-04 22:56:18 -0500

    added bootstrap classes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,25 +5,25 @@ function Card(props) {
     //if it is a person
     if (props.fourth !== undefined) {
         return (
-            <div className="card" >
-                <div className="card-body">
-                    <h5 className="card-header text-dark">{props.title}</h5>
-                    <p className="card-text">{props.secondary}</p>
-                    <p className="card-text">{props.third}</p>
-                    <a className="btn btn-primary" href={props.fourth} role="button" target="_blank">Link</a>
+                <div className="card border-dark" >
+                    <div className="card-body">
+                        <h5 className="card-header text-dark">{props.title}</h5>
+                        <p className="card-text">{props.secondary}</p>
+                        <p className="card-text">{props.third}</p>
+                        <a className="btn btn-primary" href={props.fourth} role="button" target="_blank">Link</a>
+                    </div>
                 </div>
-            </div>
         );
     } else { //if it is a film
         return (
-            <div className="card" >
-                <div className="card-body">
-                    <h5 className="card-header text-dark">{props.title}</h5>
-                    <p className="card-text">{props.secondary}</p>
-                    <p className="card-text">{props.third}</p>
-                    <Link to={`/films/${props.id}`} className="btn btn-dark" >Learn More</Link>
+                <div className="card border-dark mx-2">
+                    <div className="card-body">
+                        <h5 className="card-header text-dark">{props.title}</h5>
+                        <p className="card-text">{props.secondary}</p>
+                        <p className="card-text">{props.third}</p>
+                        <Link to={`/films/${props.id}`} className="btn btn-dark" >Learn More</Link>
+                    </div>
                 </div>
-            </div>
         );
     }
 }
